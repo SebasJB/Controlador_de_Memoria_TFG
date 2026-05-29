09ae15a5dd2161a8445faf6a20cafdf15f6ba079
SebasJB <sbarrantes0909@gmail.com>
2026-05-29 06:17:44 -0600

    Metricas

---
+++ Ambiente/Metricas/verdiLog/verdi.cmd
@@ -0,0 +1,4 @@
+wvCreateWindow
+wvOpenFile -win $_nWave2 \
+           {/mnt/vol_NFS_rh003/estudiantes/TFG_Sebastian_Barrantes_2026/Controlador_de_Memoria_TFG/Ambiente/Metricas/tb_mem_ctrl.vcd}
+debExit
